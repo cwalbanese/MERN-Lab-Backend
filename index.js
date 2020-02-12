e8dc916aac66e314e5c315781e38d142aef3542f
cwalbanese <631gti@gmail.com>
2020-02-12 16:25:24 -0500

    Added CORS
    Added cors to api

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
