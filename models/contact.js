51d6e3c6fbcf7a0f8c67dc36f8ba54d4a34bdfa5
cwalbanese <631gti@gmail.com>
2020-02-13 13:56:34 -0500

    Restructured Schema
    Restructured Schema to remove address object

--- models/contact.js
+++ models/contact.js
@@ -4,12 +4,10 @@ const ContactSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: String,
   email: String,
-  address: {
-    street: String,
-    city: String,
-    state: String,
-    zipcode: String
-  },
+  street: String,
+  city: String,
+  state: String,
+  zipcode: String,
   imageUrl: String,
   title: String,
   note: String
